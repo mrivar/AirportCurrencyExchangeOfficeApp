2dd92f94cce7d29ae012475466ec7c3d3a1fddc5
mrivar <martin7557@gmail.com>
2018-09-11 17:19:02 +0200

    Style header

--- src/client/Header.js
+++ src/client/Header.js
@@ -25,12 +25,15 @@ export default class Header extends React.Component {
   render() {
     return(
       <div id="headerContainer">
-        <p>Airport Currency Exchange Office App</p>
+        <h1>Airport Currency Exchange Office</h1>
         <section>
           <HeaderButton buttonName="Home" />
           <HeaderButton buttonName="Admin" />
           <HeaderLogo />
         </section>
+        <svg width='100%' height='100%' viewBox="0 0 100 100" preserveAspectRatio="none" >
+          <polygon points="100,0 100,40 5,100 0,70" />
+        </svg>
       </div>
     )
   }
