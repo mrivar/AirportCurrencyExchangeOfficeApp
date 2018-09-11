b08d7dcef299ef77e1d32c4da95c2297dc14aea6
mrivar <martin7557@gmail.com>
2018-09-11 21:43:37 +0200

    Reorganize css files

--- src/client/Header.js
+++ src/client/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-import "./header.css";
+import "./css/header.css";
 
 export class HeaderButton extends React.Component {
   render() {
--- src/client/HomePage.js
+++ src/client/HomePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./styles.css";
+import "./css/styles.css";
 
 export class CurrencyRow extends React.Component {
   constructor(props) {
